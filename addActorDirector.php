2fa504dfcf77b2e49d20cc95a72e36afe2e4f54b
Eric Hertz <emhertz@ucla.edu>
2011-04-23 23:41:42 -0700

    wrote some PHP, escaped strings, checked some input

--- addActorDirector.php
+++ addActorDirector.php
@@ -6,11 +6,46 @@ require($DOCUMENT_ROOT . "./menu_bar.html");
 ?>
 <title>CS 143 Movie Database</title>
 
+<?php
+	$identity = $_POST["identity"];
+	$first = $_POST["first"];
+	$last = $_POST["last"];
+	$sex = $_POST["sex"];
+	$dob = $_POST["dob"];
+	$dod = $_POST["dod"];
+
+	$db_connection = mysql_connect("localhost", "cs143", "");
+	if(!$db_connection) {
+		$err_msg = mysql_error($db_connection);
+		print "Connection failed: $errmsg <br/>";
+		exit(1);
+	}
+
+	mysql_select_db("CS143", $db_connection);
+
+	# Sanitize input
+	$identity = mysql_real_escape_string($identity, $db_connection);
+	$first = mysql_real_escape_string($first, $db_connection);
+	$last = mysql_real_escape_string($last, $db_connection);
+	$sex = mysql_real_escape_string($sex, $db_connection);
+	$dob = mysql_real_escape_string($dob, $db_connection);
+	$dod = mysql_real_escape_string($dod, $db_connection);
+
+	if($identity == "Actor") {
+		print("Inserting into Actor.");
+	} else if($identity == "Director") {
+		print("Inserting into Director.");
+	} else {
+		print("Not doing anything.");
+	}		
+
+?>
+
 <link rel="stylesheet" type="text/css" media="all" href="searchsm.css">
 <table align="center" width="650" class="searchtablesm" bgcolor="0038A8">
 	<tr bgcolor="0038A8">
 		<td align="center"><font color="FFFFFF">
-			<form action="./addActorDirector.php" method="GET">
+			<form action="./addActorDirector.php" method="POST">
 				Identity:
 			<input type="radio" name="identity" value="Actor" checked="true">Actor
 			<input type="radio" name="identity" value="Director">Director<br/>
