435770377825170971fc7f3805a07a5feaeb3c49
Eric Hertz <emhertz@ucla.edu>
2011-04-27 01:45:55 -0700

    updated menu bar, added pages that will show the actors and movies in a list. TODO: add movie.php and actor.php to receive the POSTs from the show*Info pages and display the information in a pretty format. Add search. Add database interactions for the adding info pages.

---
+++ showActorInfo.php
@@ -0,0 +1,50 @@
+<html>
+<body>
+
+<?php
+require($DOCUMENT_ROOT . "./menu_bar.html");
+?>
+<title>CS 143 Movie Database</title>
+
+<?php
+
+	$db_connection = mysql_connect("localhost", "cs143", "");
+	if(!$db_connection) {
+		$err_msg = mysql_error($db_connection);
+		print "Connection failed: $errmsg <br/>";
+		exit(1);
+	}
+
+	mysql_select_db("CS143", $db_connection);
+
+	$result = mysql_query("SELECT * FROM Actor ORDER BY last", $db_connection);
+	if(!$result) {
+		$message = "Invalid query: " . mysql_error() . "\n";
+		die($message);
+	}
+?>
+
+
+<link rel="stylesheet" type="text/css" media="all" href="searchsm.css">
+<table align="center" width="650" class="searchtablesm" bgcolor="0038A8">
+	<tr bgcolor="0038A8">
+		<td align="center"><font color="FFFFFF">
+			<form action="./actor.php" method="POST">
+			<select name="movie">
+			<?php
+			while($row = mysql_fetch_assoc($result)) {
+				echo "<option value='" . $row['id'] . "'>";
+				echo $row['first'] . " " . $row['last'];
+				echo "</option>";
+			}
+			?>
+			</option>
+			</select>
+			<input type="submit" value="Search"/>
+			</form>
+			<hr/></font>
+		</td>
+	</tr>
+</table>
+</body>
+</html> 
